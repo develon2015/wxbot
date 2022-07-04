ad9ac2baa22007ce27f4681b88cf3c0cce24bd7a
develon2015 <302615249@qq.com>
2022-07-04 17:23:58 +0800

    stash

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,51 @@
 #include <stdio.h>
 #include <Windows.h>
 
+// 消息换行
+void KeyCtrlEnter(HWND hwnd) {
+    INPUT inputs[1] = {};
+    // Ctrl down
+    ZeroMemory(inputs, sizeof(inputs));
+    inputs[0].type = INPUT_KEYBOARD;
+    inputs[0].ki.wVk = VK_LCONTROL;
+    inputs[0].ki.dwFlags = KEYEVENTF_EXTENDEDKEY;
+    SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+    Sleep(100); // 等待GetKeyState可以获取到按键状态
+    // Enter down
+    SendMessageW(hwnd, WM_KEYDOWN, (WPARAM)VK_RETURN, (LPARAM)0);
+    // Enter up
+    SendMessageW(hwnd, WM_KEYUP, (WPARAM)VK_RETURN, (LPARAM)0);
+    // Ctrl up
+    ZeroMemory(inputs, sizeof(inputs));
+    inputs[0].type = INPUT_KEYBOARD;
+    inputs[0].ki.wVk = VK_LCONTROL;
+    inputs[0].ki.dwFlags = KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP;
+    SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+}
+
 void SendText(HWND hwnd, LPCWSTR text)
 {
     size_t le = wcslen(text);
     for (int i = 0; i < le; i++)
     {
-        SendMessageW(hwnd, WM_CHAR, (WPARAM)text[i], (LPARAM)0);
+        if (text[i] == L'\n')
+        {
+            KeyCtrlEnter(hwnd);
+        }
+        else
+        {
+            SendMessageW(hwnd, WM_CHAR, (WPARAM)text[i], (LPARAM)0);
+        }
         Sleep(1);
     }
     SendMessageW(hwnd, WM_CHAR, (WPARAM)L'\0', (LPARAM)0);
-    Sleep(200);
+    // Sleep(200);
     SendMessageW(hwnd, WM_KEYDOWN, (WPARAM)VK_RETURN, (LPARAM)0);
+    SendMessageW(hwnd, WM_KEYUP, (WPARAM)VK_RETURN, (LPARAM)0);
 }
 
-void MouseLeftClick() {
+void MouseLeftClick()
+{
     INPUT inputs[2] = {};
     ZeroMemory(inputs, sizeof(inputs));
 
@@ -30,7 +61,8 @@ void MouseLeftClick() {
 }
 
 // 保证窗口置顶, 直接模拟鼠标点击输入框
-void SetTextFocus(HWND hwnd) {
+void SetTextFocus(HWND hwnd)
+{
     RECT rect = {0};
     GetWindowRect(hwnd, &rect);
     // printf("%d %d %d %d\n", rect.left, rect.top, rect.right, rect.bottom);
@@ -40,20 +72,25 @@ void SetTextFocus(HWND hwnd) {
 }
 
 // 重新打开窗口, 这会自动获得输入焦点
-void SetWXTextFocus(HWND chatWnd) {
+void SetWXTextFocus(HWND chatWnd)
+{
     ShowWindow(chatWnd, SW_HIDE);
-    ShowWindow(chatWnd, SW_SHOW);
+    ShowWindow(chatWnd, SW_SHOWDEFAULT);
     SetForegroundWindow(chatWnd);
+    // SetWindowPos(chatWnd, HWND_TOP, 600, 200, 0, 0, SWP_NOSENDCHANGING | SWP_NOSIZE);
+    Sleep(100);
 }
 
-void CallAudio(HWND hwnd) {
+void CallAudio(HWND hwnd)
+{
     RECT rect = {0};
     GetWindowRect(hwnd, &rect);
     SetCursorPos(rect.right - 65, rect.bottom - 120);
     MouseLeftClick();
 }
 
-void CallVideo(HWND hwnd) {
+void CallVideo(HWND hwnd)
+{
     RECT rect = {0};
     GetWindowRect(hwnd, &rect);
     SetCursorPos(rect.right - 36, rect.bottom - 120);
@@ -63,38 +100,62 @@ void CallVideo(HWND hwnd) {
 int usage()
 {
     HWND chatWnd = FindWindowA("ChatWnd", NULL);
-    // SetWXTextFocus(chatWnd);
-    SetTextFocus(chatWnd);
-    SendText(chatWnd, L"在吗?");
-    // CallAudio(chatWnd);
-    // CallVideo(chatWnd);
+    SetWXTextFocus(chatWnd);
+    if (GetForegroundWindow() == chatWnd)
+    {
+        SendText(chatWnd, L"在吗?");
+        // CallAudio(chatWnd);
+        // CallVideo(chatWnd);
+    }
+    else
+    {
+        printf("获取输入焦点失败\n");
+    }
     return 0;
 }
 
 // #define DllExport   __declspec( dllexport )
 // 静态链接不需要申明导出符号
-#define DllExport   
+#define DllExport
 
-extern "C"
-DllExport
-void wx_send(LPCWSTR text) {
+extern "C" DllExport void wx_send(LPCWSTR text)
+{
     HWND chatWnd = FindWindowA("ChatWnd", NULL);
-    SetTextFocus(chatWnd);
+    SetWXTextFocus(chatWnd);
+    if (GetForegroundWindow() != chatWnd)
+    {
+        return;
+    }
     SendText(chatWnd, text);
 }
 
-extern "C"
-DllExport
-void wx_audio() {
+extern "C" DllExport void wx_audio()
+{
     HWND chatWnd = FindWindowA("ChatWnd", NULL);
-    SetTextFocus(chatWnd);
+    SetWXTextFocus(chatWnd);
+    if (GetForegroundWindow() != chatWnd)
+    {
+        return;
+    }
     CallAudio(chatWnd);
 }
 
-extern "C"
-DllExport
-void wx_video() {
+extern "C" DllExport void wx_video()
+{
     HWND chatWnd = FindWindowA("ChatWnd", NULL);
-    SetTextFocus(chatWnd);
+    SetWXTextFocus(chatWnd);
+    if (GetForegroundWindow() != chatWnd)
+    {
+        return;
+    }
     CallVideo(chatWnd);
+}
+
+int main()
+{
+    wx_send(LR"(
+        你
+        好
+    )");
+    return 0;
 }
